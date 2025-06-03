f2b43726262f5d99e622b514c0a4c84a7fc66076
Ponomarev Mikhail <mvponomarevwork@gmail.com>
2025-06-03 15:17:29 +0700

    Выполнена домашка по работе с файлами( кроме части с проверкой JSON-файла)

--- src/test/java/org/example/autotesting/downloadUploadFile.java
+++ src/test/java/org/example/autotesting/downloadUploadFile.java
@@ -12,6 +12,7 @@ import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
 import org.openqa.selenium.io.Zip;
 
 import java.io.*;
@@ -23,39 +24,37 @@ import java.util.zip.ZipInputStream;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class downloadUploadFile
-{
+public class downloadUploadFile {
     private ClassLoader cl = downloadUploadFile.class.getClassLoader();  // Необходим, что бы читать файлы из resources
+
     @BeforeAll
-    public static void Before()
-    {
+    public static void Before() {
 
 
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
-       // Configuration.fileDownload = FileDownloadMode.PROXY; // Используется, когда нет href( но лучше не использовать)
+        // Configuration.fileDownload = FileDownloadMode.PROXY; // Используется, когда нет href( но лучше не использовать)
     }
 
     @Test
-    void downloadFile  () throws Exception  { //Это означает, что в процессе работы метод может "выбросить" исключение,
+    void downloadFile() throws Exception { //Это означает, что в процессе работы метод может "выбросить" исключение,
         // которое указано после слова throws(обязательно делать)
-    open("https://github.com/GarmonistAlesha/RubiusHomeWork/blob/main/README.md");
-    File download = $("a[href*= '/raw/refs/heads/main/README.md']").download();//
+        open("https://github.com/GarmonistAlesha/RubiusHomeWork/blob/main/README.md");
+        File download = $("a[href*= '/raw/refs/heads/main/README.md']").download();//
         // Загрузка файла нажатием на клавишу Raw в GitHub
         // download работает только с атрибутами, у которых есть a[href(иногда нет такого)
         // Объект типа File - это абстракция над путями к файлу (загруженый файл находится в build/downloads/sessionid запуска)
-       try (InputStream is = new FileInputStream(download))
-       {
-          byte[] bytes = is.readAllBytes();
-          String fileAsString = new String(bytes, StandardCharsets.UTF_8); // Конструкция проверки скаченного файла на совпадение текста в нем с assertTrue
-           Assertions.assertTrue(fileAsString.contains("RubiusHomeWork"));
-       }
+        try (InputStream is = new FileInputStream(download)) {
+            byte[] bytes = is.readAllBytes();
+            String fileAsString = new String(bytes, StandardCharsets.UTF_8); // Конструкция проверки скаченного файла на совпадение текста в нем с assertTrue
+            Assertions.assertTrue(fileAsString.contains("RubiusHomeWork"));
+        }
 
     }
+
     //Все файлы, с которыми работаем, необходимо помещать в папку resources в проекте
     @Test
-    void uploadFile ()
-    {
+    void uploadFile() {
         open("https://tus.io/demo.html");
         $("input[type = 'file']").uploadFromClasspath(("img/1.png")); //Загрузка файла с папки resources в
         // директории test java(сокращенно) (selenid метод), uploadFile лучше не использовать
@@ -71,7 +70,7 @@ public class downloadUploadFile
         File download = $("a[href *= 'junit-user-guide-5.12.2.pdf']").download();
         PDF pdf = new PDF(download);
         System.out.println();
-        Assertions.assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein",pdf.author);
+        Assertions.assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein", pdf.author);
 
     }
 
@@ -83,7 +82,7 @@ public class downloadUploadFile
         XLS xls = new XLS(download);
         System.out.println();
         Assertions.assertTrue(xls.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue().startsWith("Внешний идентификатор для импорта"));
-        //xls.excel.getSheetAt(0)- страница таблицы.getRow(0)- столбец таблицы.getCell(0)-строка таблицы.getStringCellValue()
+        //xls.excel.getSheetAt(0)- страница таблицы.getRow(0)- строка таблицы.getCell(0)-столбец таблицы.getStringCellValue()- преобразование в текст содержимого таблицы
         //Возникает ошибка в большинстве случаев, надо сделать пару манипуляций:
 //      в терминале вбить gradle dependencies
 //      найти версию ошибки (тут была org.apache.poi:poi:3.9*(приходит из xlstest) -> 5.3.0(а ищет в pdftest))
@@ -93,50 +92,107 @@ public class downloadUploadFile
     }
 
     @Test
-    void workCsvFile() throws Exception
-    {
-       try (InputStream is = cl.getResourceAsStream("TestData/testCsv.csv");
-            InputStreamReader isr = new InputStreamReader(is))
+    void workCsvFile() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("TestData/testCsv.csv");
+             InputStreamReader isr = new InputStreamReader(is))
         // Необходимо подключить библиотеку OpenCsv
         {
             CSVReader csvReader = new CSVReader(isr);
             List<String[]> content = csvReader.readAll();//Каждая строчка в CSV это массив string List - это список строчек
-            Assertions.assertArrayEquals(new String[]{"QuotaAmount","StartDate","OwnerName","Username"},content.get(0));
+            Assertions.assertArrayEquals(new String[]{"QuotaAmount", "StartDate", "OwnerName", "Username"}, content.get(0));
             // Сравнение данных в 1 строчке CSV файла
         }
     }
 
     @Test
-    void workZipfile () throws Exception
-    {
+    void workZipfile() throws Exception {
         try (InputStream is = cl.getResourceAsStream("TestData/testjpg.zip");
-             ZipInputStream zs = new ZipInputStream(is))
-        {
+             ZipInputStream zs = new ZipInputStream(is)) {
             ZipEntry entry;
             while ((entry = zs.getNextEntry()) != null) //Цикл распоковки файлов с архива( повторяет операцию до тех пор,
-                // пока есть файлы)
+            // пока есть файлы)
             {
-            Assertions.assertTrue(entry.getName().contains("testjpg.JPG"));
+                Assertions.assertTrue(entry.getName().contains("testjpg.JPG"));
             }
         }
 
     }
+
     @Test
-    void workJsonFile () throws Exception //Метод работы Json файла и проверки (необходимо подключить библиотеку
+    void workJsonFile() throws Exception //Метод работы Json файла и проверки (необходимо подключить библиотеку
     // в buildgradle com.google.code.gson:gson:2.13.1 maven(найти)
     {
         Gson gson = new Gson();
         try (InputStream is = cl.getResourceAsStream("TestData/testJson.json");
-             InputStreamReader br = new InputStreamReader(is))
-        {
+             InputStreamReader br = new InputStreamReader(is)) {
             JsonObject jsonObject = gson.fromJson(br, JsonObject.class);
-            Assertions.assertEquals("John",jsonObject.get("name").getAsString());
-            Assertions.assertEquals(30,jsonObject.get("age").getAsInt());
-            Assertions.assertEquals("New York",jsonObject.get("city").getAsString());
+            Assertions.assertEquals("John", jsonObject.get("name").getAsString());
+            Assertions.assertEquals(30, jsonObject.get("age").getAsInt());
+            Assertions.assertEquals("New York", jsonObject.get("city").getAsString());
+            Assertions.assertTrue(jsonObject.getAsJsonArray("hobbies").isJsonArray()); //проверка, что hobbies является массивом
+
             //Лучше создать отдельный класс для проверки Json
         }
     }
+
+    @Test
+    void homeWorkCsv() throws Exception {
+        try (InputStream hw = cl.getResourceAsStream("TestData/homeWork.zip");
+             ZipInputStream csv = new ZipInputStream(hw)) {
+            ZipEntry entry;
+            while ((entry = csv.getNextEntry()) != null) //Цикл распоковки файлов с архива( повторяет операцию до тех пор,
+            // пока есть файлы)
+            {
+                if (entry.getName().equals("testCsv1.csv"))
+                {
+                    CSVReader csvReader = new CSVReader(new InputStreamReader(hw));
+                    List<String[]> content = csvReader.readAll();//Каждая строчка в CSV это массив string List - это список строчек
+                    Assertions.assertTrue(entry.getName().contains("annual-enterprise-survey-2023-financial-year-provisional.csv"));
+                    Assertions.assertArrayEquals(new String[]{"Year", "Industry_aggregation_NZSIOC", "Industry_code_NZSIOC", "Industry_name_NZSIOC","Units"}, content.get(0));
+                    Assertions.assertArrayEquals(new String[]{"2023", "ILevel 1", "99999", "All industries,Dollars (millions)","H01"}, content.get(1));
+                }
+            }
+        }
+        }
+    @Test
+    void homeWorkPdf() throws Exception {
+        try (InputStream hw = cl.getResourceAsStream("TestData/homeWork.zip");
+             ZipInputStream pdf = new ZipInputStream(hw)) {
+            ZipEntry entry;
+            while ((entry = pdf.getNextEntry()) != null)
+            {
+                if (entry.getName().equals("Ponomarev Mikhail.pdf"))
+                {
+
+                PDF pdf1 = new PDF(pdf);
+                Assertions.assertTrue(entry.getName().contains("Ponomarev Mikhail.pdf"));
+                pdf1.text.contains("mvponomarevwork@gmail.com");
+                }
+            }
+        }
+    }
+    @Test
+    void homeWorkXls() throws Exception {
+        try (InputStream hw = cl.getResourceAsStream("TestData/homeWork.zip");
+             ZipInputStream xls = new ZipInputStream(hw)) {
+            ZipEntry entry;
+            while ((entry = xls.getNextEntry()) != null)
+            {
+                if (entry.getName().equals("import_ou_xls.xls"))
+                {
+
+                    XLS xls1 = new XLS(xls);
+                    Assertions.assertTrue(entry.getName().contains("import_ou_xls.xls"));
+                    Assertions.assertTrue(xls1.excel.getSheetAt(0).getRow(2).getCell(2).getStringCellValue().startsWith("Маректинг и реклама"));
+
+                }
+            }
+        }
+    }
+
 }
+
+
 
 
 
